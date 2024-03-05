46d73f05f0fa54ec0e1d02a0694657fbcca5713b
ANT <bit0316@naver.com>
2024-03-05 20:10:15 +0900

    [Bronze V] Title: 사칙연산, Time: 84 ms, Memory: 5532 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.cs
@@ -0,0 +1,19 @@
+public class Program
+{
+    static void Main(string[] args)
+    {
+        int A;
+        int B;
+        string[] input;
+
+        input = Console.ReadLine().Split();
+        A = Convert.ToInt32(input[0]);
+        B = Convert.ToInt32(input[1]);
+
+        Console.WriteLine(A + B);
+        Console.WriteLine(A - B);
+        Console.WriteLine(A * B);
+        Console.WriteLine(A / B);
+        Console.WriteLine(A % B);
+    }
+}
