8ce2e822b2dbd31fc2cc69442cccf93ccb39e2d1
ANT <bit0316@naver.com>
2024-03-05 20:00:41 +0900

    [Bronze V] Title: A+B, Time: 64 ms, Memory: 5528 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.cs
@@ -0,0 +1,17 @@
+using System;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        int A;
+        int B;
+        string[] input;
+
+        input = Console.ReadLine().Split();
+        A = Convert.ToInt32(input[0]);
+        B = Convert.ToInt32(input[1]);
+
+        Console.WriteLine(A + B);
+    }
+}
