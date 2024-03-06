a5e7c605ab165761221f7e3966803f01b9b99c60
ANT <bit0316@naver.com>
2024-03-07 00:52:28 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 280 ms, Memory: 6568 KB -BaekjoonHub

---
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.cs
@@ -0,0 +1,18 @@
+public class Program
+{
+    static void Main(string[] args)
+    {
+        int num;
+
+        num = int.Parse(Console.ReadLine());
+
+        for (int row = 1; row <= num; ++row)
+        {
+            for (int col = 1; col <= row; ++col)
+            {
+                Console.Write("*");
+            }
+            Console.WriteLine();
+        }
+    }
+}
