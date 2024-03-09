1171fde674d10ebd655c83474db8a5b4c3944a3e
ANT <bit0316@naver.com>
2024-03-09 19:23:48 +0900

    [Bronze V] Title: 아스키 코드, Time: 64 ms, Memory: 5452 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.cs
@@ -0,0 +1,11 @@
+public class Program
+{
+    static void Main(string[] args)
+    {
+        string input;
+
+        input = Console.ReadLine();
+
+        Console.WriteLine((int)input[0]);
+    }
+}
