8edf017fee81cebb1eb57eeba48109e6b2ef90eb
ANT <bit0316@naver.com>
2024-03-15 03:52:47 +0900

    [Bronze IV] Title: 알고리즘 수업 - 알고리즘의 수행 시간 2, Time: 64 ms, Memory: 5488 KB -BaekjoonHub

---
+++ 백준/Bronze/24263. 알고리즘 수업 － 알고리즘의 수행 시간 2/알고리즘 수업 － 알고리즘의 수행 시간 2.cs
@@ -0,0 +1,12 @@
+public class Program
+{
+    static void Main(string[] args)
+    {
+        int num;
+
+        num = int.Parse(Console.ReadLine());
+
+        Console.WriteLine(num);
+        Console.WriteLine(1);
+    }
+}
