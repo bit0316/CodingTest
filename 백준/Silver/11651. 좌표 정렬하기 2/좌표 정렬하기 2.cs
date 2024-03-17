2e0260d513e797903b7a0aa623fae35f0e7afea6
ANT <bit0316@naver.com>
2024-03-17 10:33:30 +0900

    [Silver V] Title: 좌표 정렬하기 2, Time: 284 ms, Memory: 23636 KB -BaekjoonHub

---
+++ 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.cs
@@ -0,0 +1,54 @@
+public class Program
+{
+    static StreamReader sr = new StreamReader(Console.OpenStandardInput());
+    static StreamWriter sw = new StreamWriter(Console.OpenStandardOutput());
+
+    static void Main(string[] args)
+    {
+        int size;
+        int[][] arr;
+
+        size = int.Parse(sr.ReadLine());
+        arr = new int[size][];
+
+        SetArray(arr);
+        arr = SortArray(arr);
+        PrintArray(arr);
+
+        sr.Close();
+        sw.Close();
+    }
+
+    static void SetArray(int[][] arr)
+    {
+        int posX;
+        int posY;
+        int size = arr.Length;
+        string[] input;
+
+        for (int i = 0; i < size; ++i)
+        {
+            input = sr.ReadLine().Split();
+            posX = int.Parse(input[0]);
+            posY = int.Parse(input[1]);
+            arr[i] = new int[] { posX, posY };
+        }
+    }
+
+    static int[][] SortArray(int[][] arr)
+    {
+        return arr.OrderBy(x => x[1]).ThenBy(x => x[0]).ToArray();
+    }
+
+    static void PrintArray(int[][] arr)
+    {
+        int size = arr.Length;
+
+        for (int i = 0; i < size; ++i)
+        {
+            sw.WriteLine($"{arr[i][0]} {arr[i][1]}");
+        }
+
+        sw.Flush();
+    }
+}
