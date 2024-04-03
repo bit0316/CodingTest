3e3b6f70440a2438a065dd7aaf3e0ac3e91fca7c
ANT <bit0316@naver.com>
2024-04-03 22:24:16 +0900

    [Silver III] Title: N과 M (3), Time: 1028 ms, Memory: 19652 KB -BaekjoonHub

---
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.cs
@@ -0,0 +1,43 @@
+public class Program
+{
+    static StreamReader SR = new StreamReader(Console.OpenStandardInput());
+    static StreamWriter SW = new StreamWriter(Console.OpenStandardOutput());
+
+    static int[] result;
+    static int numN;
+    static int numM;
+
+    static void Main(string[] args)
+    {
+        int[] input;
+
+        input = Array.ConvertAll(SR.ReadLine().Split(), int.Parse);
+        numN = input[0];
+        numM = input[1];
+        result = new int[numM];
+
+        DFS(0);
+
+        SR.Close();
+        SW.Close();
+    }
+
+    static void DFS(int depth)
+    {
+        if (depth == numM)
+        {
+            for (int i = 0; i < numM; ++i)
+            {
+                SW.Write($"{result[i]} ");
+            }
+            SW.WriteLine();
+            return;
+        }
+
+        for (int i = 1; i <= numN; ++i)
+        {
+            result[depth] = i;
+            DFS(depth + 1);
+        }
+    }
+}
