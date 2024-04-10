64fd1396acd1bf407b39d3509e1c451c69708752
ANT <bit0316@naver.com>
2024-04-10 21:28:56 +0900

    [Bronze V] Title: We love kriii, Time: 52 ms, Memory: 5352 KB -BaekjoonHub

---
+++ 백준/Bronze/10718. We love kriii/We love kriii.cs
@@ -0,0 +1,14 @@
+public class Program
+{
+    static StreamReader SR = new StreamReader(Console.OpenStandardInput());
+    static StreamWriter SW = new StreamWriter(Console.OpenStandardOutput());
+
+    static void Main(string[] args)
+    {
+        SW.WriteLine("강한친구 대한육군");
+        SW.WriteLine("강한친구 대한육군");
+
+        SR.Close();
+        SW.Close();
+    }
+}
