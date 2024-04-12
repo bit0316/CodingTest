1a179f54f131853c7b4003f8e9f87f2f4204ea31
ANT <bit0316@naver.com>
2024-04-13 07:49:41 +0900

    [Silver III] Title: 2×n 타일링 2, Time: 72 ms, Memory: 5492 KB -BaekjoonHub

---
+++ 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.cs
@@ -0,0 +1,38 @@
+public class Program
+{
+    static StreamReader SR = new StreamReader(Console.OpenStandardInput());
+    static StreamWriter SW = new StreamWriter(Console.OpenStandardOutput());
+
+    static void Main(string[] args)
+    {
+        int num;
+        int result;
+
+        num = int.Parse(SR.ReadLine());
+
+        result = GetResult(num);
+        PrintResult(result);
+
+        SR.Close();
+        SW.Close();
+    }
+
+    static int GetResult(int num)
+    {
+        int[] arr = new int[num + 2];
+
+        arr[1] = 1;
+        arr[2] = 3;
+        for (int i = 3; i <= num; ++i)
+        {
+            arr[i] = (arr[i - 1] + arr[i - 2] * 2) % 10007;
+        }
+
+        return arr[num];
+    }
+
+    static void PrintResult(int result)
+    {
+        SW.WriteLine(result);
+    }
+}
