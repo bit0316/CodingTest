815277568c6f7fcaa1921e82b5f46c8378ecbbb6
YeongHo Kim <bit0316@naver.com>
2024-05-20 20:06:14 +0900

    [Silver I] Title: 카드 구매하기, Time: 68 ms, Memory: 5852 KB -BaekjoonHub

---
+++ 백준/Silver/11052. 카드 구매하기/카드 구매하기.cs
@@ -0,0 +1,35 @@
+public class Program
+{
+    static StreamReader SR = new StreamReader(Console.OpenStandardInput());
+    static StreamWriter SW = new StreamWriter(Console.OpenStandardOutput());
+
+    static int size;
+    static int[] prices;
+    static int[] totals;
+    static int[] input;
+
+    static void Main(string[] args)
+    {
+        size = int.Parse(SR.ReadLine());
+        prices = new int[size + 1];
+        totals = new int[size + 1];
+
+        input = Array.ConvertAll(SR.ReadLine().Split(), int.Parse);
+        for (int i = 1; i <= size; ++i)
+        {
+            prices[i] = input[i - 1];
+        }
+
+        for (int i = 1; i <= size; ++i)
+        {
+            for (int j = 1; j <= i; ++j)
+            {
+                totals[i] = Math.Max(totals[i], totals[i - j] + prices[j]);
+            }
+        }
+        SW.WriteLine(totals[size]);
+
+        SR.Close();
+        SW.Close();
+    }
+}
