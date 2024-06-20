1cbd4f8ccafe1df428a54983eec764a1e5902124
YeongHo Kim <bit0316@naver.com>
2024-06-20 17:03:21 +0900

    [Bronze V] Title: A+B - 2, Time: 92 ms, Memory: 5484 KB -BaekjoonHub

---
+++ 백준/Bronze/2558. A＋B － 2/A＋B － 2.cs
@@ -0,0 +1,12 @@
+public class Program
+{
+    static void Main(string[] args)
+    {
+        int result = 0;
+
+        result += int.Parse(Console.ReadLine());
+        result += int.Parse(Console.ReadLine());
+
+        Console.WriteLine(result);
+    }
+}
