62d3989e33d3bb58527ebdf997cd0eef65f32f52
YeongHo Kim <bit0316@naver.com>
2024-07-17 08:31:06 +0900

    [Gold II] Title: 피보나치 수 6, Time: 64 ms, Memory: 5648 KB -BaekjoonHub

---
+++ 백준/Gold/11444. 피보나치 수 6/피보나치 수 6.cs
@@ -0,0 +1,55 @@
+public class Program
+{
+    static StreamReader SR = new StreamReader(Console.OpenStandardInput());
+    static StreamWriter SW = new StreamWriter(Console.OpenStandardOutput());
+
+    static int MOD = 1000000007;
+
+    static Dictionary<long, long> dict = new Dictionary<long, long>();
+
+    static void Main(string[] args)
+    {
+        long num;
+        long result;
+
+        num = long.Parse(SR.ReadLine());
+
+        dict.Add(0, 0);
+        dict.Add(1, 1);
+        result = Fibo(num);
+
+        PrintResult(result);
+
+        SR.Close();
+        SW.Close();
+    }
+
+    static long Fibo(long num)
+    {
+        long fiboA;
+        long fiboB;
+
+        if (!dict.ContainsKey(num))
+        {
+            if (num % 2 == 0)
+            {
+                fiboA = Fibo(num / 2);
+                fiboB = Fibo(num / 2 - 1);
+                dict.Add(num, (2 * fiboB + fiboA) * fiboA % MOD);
+            }
+            else
+            {
+                fiboA = Fibo(num / 2);
+                fiboB = Fibo(num / 2 + 1);
+                dict.Add(num, (fiboA * fiboA + fiboB * fiboB) % MOD);
+            }
+        }
+
+        return dict[num];
+    }
+
+    static void PrintResult(long result)
+    {
+        SW.WriteLine(result);
+    }
+}
