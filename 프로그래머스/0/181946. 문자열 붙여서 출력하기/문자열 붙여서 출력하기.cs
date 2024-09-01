ee9c53f38750419aa1eed1eb765a801c49557060
YeongHo Kim <bit0316@naver.com>
2024-09-02 07:55:51 +0900

    [level 0] Title: 문자열 붙여서 출력하기, Time: 16.50 ms, Memory: 16.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181946. 문자열 붙여서 출력하기/문자열 붙여서 출력하기.cs
@@ -0,0 +1,13 @@
+using System;
+
+public class Example
+{
+    public static void Main()
+    {
+        string[] input = Console.ReadLine().Split();
+        string str1 = input[0];
+        string str2 = input[1];
+        
+        Console.WriteLine(str1 + str2);
+    }
+}
