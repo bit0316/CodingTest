2bf780b5d82fad3c30d5af25459fc59bcf50df34
YeongHo Kim <bit0316@naver.com>
2024-09-02 07:56:45 +0900

    [level 0] Title: 문자열 돌리기, Time: 15.82 ms, Memory: 16.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181945. 문자열 돌리기/문자열 돌리기.cs
@@ -0,0 +1,14 @@
+using System;
+
+public class Example
+{
+    public static void Main()
+    {
+        string input = Console.ReadLine();
+        
+        for (int i = 0; i < input.Length; ++i)
+        {
+            Console.WriteLine(input[i]);
+        }
+    }
+}
