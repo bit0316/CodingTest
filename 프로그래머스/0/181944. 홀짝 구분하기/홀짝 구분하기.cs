4b0bca30815f716aed425a29f863447b9eb2df5d
YeongHo Kim <bit0316@naver.com>
2024-09-02 08:02:29 +0900

    [level 0] Title: 홀짝 구분하기, Time: 18.73 ms, Memory: 16.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181944. 홀짝 구분하기/홀짝 구분하기.cs
@@ -0,0 +1,11 @@
+using System;
+
+public class Example
+{
+    public static void Main()
+    {
+        int num = int.Parse(Console.ReadLine());
+        
+        Console.WriteLine($"{num} is {num % 2 == 0 ? "even" : "odd"}");
+    }
+}
