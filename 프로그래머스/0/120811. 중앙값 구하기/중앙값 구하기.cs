127b3673cc8432cc78543010ff5a18255d4c80fe
YeongHo Kim <bit0316@naver.com>
2024-09-03 14:20:44 +0900

    [level 0] Title: 중앙값 구하기, Time: 2.33 ms, Memory: 31.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120811. 중앙값 구하기/중앙값 구하기.cs
@@ -0,0 +1,14 @@
+using System;
+
+public class Solution
+{
+    public int solution(int[] array)
+    {
+        int answer = 0;
+        
+        Array.Sort(array);
+        answer = array[array.Length / 2];
+        
+        return answer;
+    }
+}
