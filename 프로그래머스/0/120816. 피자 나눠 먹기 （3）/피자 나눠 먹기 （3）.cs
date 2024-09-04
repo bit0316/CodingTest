fe971814c1d99a917509fc0d5d47357184534db3
YeongHo Kim <bit0316@naver.com>
2024-09-04 09:58:03 +0900

    [level 0] Title: 피자 나눠 먹기 (3), Time: 0.25 ms, Memory: 31.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120816. 피자 나눠 먹기 （3）/피자 나눠 먹기 （3）.cs
@@ -0,0 +1,16 @@
+using System;
+
+public class Solution
+{
+    public int solution(int slice, int n)
+    {
+        int answer = 0;
+        
+        while (slice * answer < n)
+        {
+            answer++;
+        }
+        
+        return answer;
+    }
+}
