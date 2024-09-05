43c3e3046445d37c78876f5c8509393b5f7a72a3
YeongHo Kim <bit0316@naver.com>
2024-09-05 19:06:02 +0900

    [level 0] Title: 배열 뒤집기, Time: 0.17 ms, Memory: 31.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120821. 배열 뒤집기/배열 뒤집기.cs
@@ -0,0 +1,17 @@
+using System;
+
+public class Solution
+{
+    public int[] solution(int[] num_list)
+    {
+        int size = num_list.Length;
+        int[] answer = new int[size];
+        
+        for (int i = 0; i < size; ++i)
+        {
+            answer[i] = num_list[size - i - 1];
+        }
+        
+        return answer;
+    }
+}
