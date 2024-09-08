16b7370e206fd419f98cb1a33ec38d1f02930b16
YeongHo Kim <bit0316@naver.com>
2024-09-08 21:37:06 +0900

    [level 0] Title: 주사위 게임 3, Time: 2.58 ms, Memory: 31.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181916. 주사위 게임 3/주사위 게임 3.cs
@@ -0,0 +1,48 @@
+using System;
+
+public class Solution
+{
+    public int solution(int a, int b, int c, int d)
+    {
+        int answer = 0;
+        int[] arr = { a, b, c, d };
+        
+        Array.Sort(arr);
+        if (arr[0] == arr[3])
+        {
+            answer = 1111 * arr[0];
+        }
+        else if (arr[0] == arr[2])
+        {
+            answer = 10 * arr[0] + arr[3];
+            answer *= answer;
+        }
+        else if (arr[1] == arr[3])
+        {
+            answer = 10 * arr[3] + arr[0];
+            answer *= answer;
+        }
+        else if (arr[0] == arr[1] && arr[2] == arr[3])
+        {
+            answer = (arr[0] + arr[2]) * (arr[2] - arr[0]);
+        }
+        else if (arr[0] == arr[1])
+        {
+            answer = arr[2] * arr[3];
+        }
+        else if (arr[1] == arr[2])
+        {
+            answer = arr[3] * arr[0];
+        }
+        else if (arr[2] == arr[3])
+        {
+            answer = arr[0] * arr[1];
+        }
+        else
+        {
+            answer = arr[0];
+        }
+        
+        return answer;
+    }
+}
