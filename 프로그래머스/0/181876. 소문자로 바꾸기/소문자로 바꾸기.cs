41ef8960f1094d6f6660ea2dbc8c43e578f53b9c
YeongHo Kim <bit0316@naver.com>
2024-09-16 09:52:00 +0900

    [level 0] Title: 소문자로 바꾸기, Time: 0.48 ms, Memory: 31.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181876. 소문자로 바꾸기/소문자로 바꾸기.cs
@@ -0,0 +1,11 @@
+using System;
+
+public class Solution
+{
+    public string solution(string myString)
+    {
+        string answer = myString.ToLower();
+        
+        return answer;
+    }
+}
