bcc506ac648d62c3b4ff5e1cafaaa7438994d380
YeongHo Kim <bit0316@naver.com>
2024-09-22 18:09:10 +0900

    [level 0] Title: 0 떼기, Time: 0.96 ms, Memory: 31.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181847. 0 떼기/0 떼기.cs
@@ -0,0 +1,11 @@
+using System;
+
+public class Solution
+{
+    public string solution(string n_str)
+    {
+        string answer = int.Parse(n_str).ToString();
+        
+        return answer;
+    }
+}
