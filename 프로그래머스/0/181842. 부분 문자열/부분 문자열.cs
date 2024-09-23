fabf1c12476bcbc33fd5d9dca3e78b16eeade038
YeongHo Kim <bit0316@naver.com>
2024-09-23 11:32:54 +0900

    [level 0] Title: 부분 문자열, Time: 0.21 ms, Memory: 31.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181842. 부분 문자열/부분 문자열.cs
@@ -0,0 +1,11 @@
+using System;
+
+public class Solution
+{
+    public int solution(string str1, string str2)
+    {
+        int answer = str2.Contains(str1) ? 1 : 0;
+        
+        return answer;
+    }
+}
