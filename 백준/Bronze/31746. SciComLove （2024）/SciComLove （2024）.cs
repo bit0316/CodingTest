7785ec81ad52e992dec98e148d2f0552e5bb3be0
YeongHo Kim <bit0316@naver.com>
2024-10-12 22:30:34 +0900

    [Bronze IV] Title: SciComLove (2024), Time: 48 ms, Memory: 5464 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/31746. SciComLove （2024）/SciComLove （2024）.cs
@@ -0,0 +1,9 @@
+public class Program
+{
+    static void Main(string[] args)
+    {
+        int size = int.Parse(Console.ReadLine());
+
+        Console.WriteLine(size % 2 == 0 ? "SciComLove" : "evoLmoCicS");
+    }
+}
