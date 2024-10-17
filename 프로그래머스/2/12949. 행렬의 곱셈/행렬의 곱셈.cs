d8cacfd53384918fcff2a2cbb6ec1a7dcaee8d69
YeongHo Kim <bit0316@naver.com>
2024-10-17 18:33:18 +0900

    [level 2] Title: 행렬의 곱셈, Time: 4.13 ms, Memory: 31.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.cs
@@ -0,0 +1,24 @@
+using System;
+
+public class Solution {
+    public int[,] solution(int[,] arr1, int[,] arr2)
+    {
+        int row = arr1.GetLength(0);
+        int col = arr2.GetLength(1);
+        int mul = arr2.GetLength(0);
+        int[,] answer = new int[row, col];
+        
+        for (int i = 0; i < row; ++i)
+        {
+            for (int j = 0; j < col; ++j)
+            {
+                for (int k = 0; k < mul; ++k)
+                {
+                    answer[i, j] += arr1[i, k] * arr2[k, j];
+                }
+            }
+        }
+        
+        return answer;
+    }
+}
